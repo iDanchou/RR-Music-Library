a580a5b21a1d1f7ba497b4b2ef790f4dd07b8ef8
Rasheen Kirkland <kirkland.rasheen@gmail.com>
2024-04-27 14:56:40 -0700

    updating progress of music library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import Gallery from "./components/gallery";
+import SearchBar from "./components/searchBar";
 
 function App() {
-  return null;
+  let [search, setSearch] = useState("");
+  let [message, setMessage] = useState("Search for an artist");
+  let [data, setData] = useState([]);
+
+  const API_URL = "https://itunes.apple.com/search?term=";
+
+  useEffect(() => {
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${search} Music`;
+        const response = await fetch(`${API_URL}${search}`);
+        const resData = await response.json();
+        if (resData.resultCount > 0) {
+          setData(resData.results);
+        } else {
+          setMessage("No results found");
+        }
+      };
+      fetchData();
+    }
+  }, [search]);
+
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearch(term);
+  };
+
+  return (
+    <div>
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      <Gallery data={data} />
+    </div>
+  );
 }
 
 export default App;
